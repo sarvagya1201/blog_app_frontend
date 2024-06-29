cabd7cc99d4c18ce7af0ca3bfd286c96c57f18ab
Sarvagya Tiwari <sarvagyatiwari7@gmail.com>
2024-06-29 13:51:42 +0530

    url from env

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-// export const URL=import.meta.env.VITE_URL
-// export const IF=import.meta.env.VITE_IF
-export const URL="https://blog-app-backend-srbp.onrender.com"
-export const IF="https://blog-app-backend-srbp.onrender.com/images/"
+export const URL=import.meta.env.VITE_URL
+export const IF=import.meta.env.VITE_IF
+// export const URL="https://blog-app-backend-srbp.onrender.com"
+// export const IF="https://blog-app-backend-srbp.onrender.com/images/"
